fb22fc36beb1e1fb700d11866e5e46f93d0650bf
Pablo Chavez <plchavez@usf.edu>
2025-02-11 16:37:39 -0500

    Task 4 completed

--- cc_example.js
+++ cc_example.js
@@ -25,4 +25,21 @@ function applyDiscount(products, discountRate) {
 
 console.log(applyDiscount(products, .1));
 
+//Task 3
+let sales = [250, 400, 150, 900, 1200]
 
+function calculateTotalRevenue(sales) {
+    return sales.reduce((total, sale) => total+sale, 0)
+};
+
+console.log(`Total Rev. 2024: $${calculateTotalRevenue(sales)}`);
+
+//Task 4 
+let employee = {name: "John Doe", salary: 5000, position: "Manager"};
+
+function updateSalary(employee, percentageIncrease) {
+    return employee.salary += employee.salary * percentageIncrease;
+    
+};
+
+console.log(updateSalary(employee, .1));
