3d59ca5be8a2ac7c03241a72000f63aa27074c72
Pablo Chavez <plchavez@usf.edu>
2025-02-11 16:06:21 -0500

    Completed task 1

---
+++ cc_example.js
@@ -0,0 +1,16 @@
+// Task 1 
+let products = [
+    {name: "Laptop", price: 1200, category: "Electronics"},
+    {name: "Phone", price: 800, category: "Electronics"},
+    {name: "Desk Chair", price: 150, category: "furniture"}
+];
+
+
+// function getProductsByCategory(products, category){
+//    return products.filter(product => product.category === category);
+// }
+
+// console.log(getProductsByCategory(products, "Electronics"));
+
+// Task 2
+
